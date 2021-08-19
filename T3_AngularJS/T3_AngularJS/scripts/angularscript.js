23ee2686f178bea5160e4875ee7d53939f5ce7c4
JabirAgd <jabiragd@gmail.com>
2021-08-19 12:18:07 +0400

    inittial Commit

---
+++ T3_AngularJS/T3_AngularJS/scripts/angularscript.js
@@ -0,0 +1,13 @@
+﻿var myApp = angular.module("myModule", []);
+
+var myController = function ($scope) {
+    var employee = {
+        firstName: 'JABIR',
+        lastName: 'AGD',
+        gender  : 'MALE'
+    };
+
+    $scope.employee = employee;
+}
+
+myApp.controller("myController", myController);
