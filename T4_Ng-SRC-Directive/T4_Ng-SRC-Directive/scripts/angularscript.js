a963372cfb312c00ad7699075fac823184365cf1
JabirAgd <jabiragd@gmail.com>
2021-08-19 13:35:02 +0400

    initial commit

---
+++ T4_Ng-SRC-Directive/T4_Ng-SRC-Directive/scripts/angularscript.js
@@ -0,0 +1,12 @@
+﻿var myApp = angular.module("myModule", []);
+
+var myController = function ($scope) {
+    var countryDetail = {
+        countryName: "India",
+        capital: "Delhi",
+        flag: '/image/flag.png'
+    };
+    $scope.countryDetail = countryDetail;
+}
+
+myApp.controller("myController", myController);
