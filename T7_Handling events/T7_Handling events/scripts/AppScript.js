3d4c229040b6718a565747235b44b5aa514c2828
JabirAgd <jabiragd@gmail.com>
2021-08-19 15:56:27 +0400

    implementation

---
+++ T7_Handling events/T7_Handling events/scripts/AppScript.js
@@ -0,0 +1,22 @@
+﻿var myApp = angular.module("myModule", []);
+
+var myController = function ($scope) {
+
+    var technologies = [
+        { name: "C#", likes: 0, dislikes: 0 },
+        { name: "ASP.NET", likes: 0, dislikes: 0 },
+        { name: "SQL", likes: 0, dislikes: 0 },
+        { name: "AngularJS", likes: 0, dislikes: 0 }
+    ];
+    $scope.technologies = technologies;
+
+    $scope.incrementLikes = function (technology) {
+        technology.likes++;
+    }
+
+    $scope.increamentDislikes = function (technology) {
+        technology.dislikes++;
+    }
+}
+
+myApp.controller("myController", myController);
