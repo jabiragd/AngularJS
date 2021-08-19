901b6116a30445dc8edcaece47cf2cdbea45172c
JabirAgd <jabiragd@gmail.com>
2021-08-19 11:52:44 +0400

    Initial commit

---
+++ T2_AngularJs/T2_AngularJs/scripts/angularscript.js
@@ -0,0 +1,7 @@
+﻿var module = angular.module("myModule", []);
+
+var myController = function ($scope) {
+    $scope.message = "WELCOME TO ANGULAR JS";
+}
+
+module.controller("myController", myController)
