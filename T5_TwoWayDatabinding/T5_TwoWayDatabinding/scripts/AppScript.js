df697b57c171e486a723d7c4560e2a750c889c94
JabirAgd <jabiragd@gmail.com>
2021-08-19 14:36:10 +0400

    initialCommit

---
+++ T5_TwoWayDatabinding/T5_TwoWayDatabinding/scripts/AppScript.js
@@ -0,0 +1,14 @@
+﻿var myApp = angular.module("myModule", []);
+
+var myController = function ($scope) {
+    
+    var employee = {
+        firstName: "John",
+        lastName: "Ben",
+        gender : "Male"
+    };
+
+    $scope.employee = employee;
+}
+
+myApp.controller("myController", myController);
