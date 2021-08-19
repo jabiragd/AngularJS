174a6abbc184e979d27f9b2b52ca016b5aeca11e
JabirAgd <jabiragd@gmail.com>
2021-08-19 14:01:37 +0400

    initialCommit

---
+++ T6_NgRepeatDirective/T6_NgRepeatDirective/scripts/AppScript.js
@@ -0,0 +1,14 @@
+﻿var myApp = angular.module("myModule", []);
+
+var myController = function ($scope) {
+    var employees = [
+        { firstName: "TestF1", lastName: "TestL1", gender: "Male" },
+        { firstName: "TestF2", lastName: "TestL2", gender: "Female" },
+        { firstName: "TestF3", lastName: "TestL3", gender: "Male" },
+        { firstName: "TestF4", lastName: "TestL4", gender: "Female" }
+    ];
+
+    $scope.employees = employees;
+}
+
+myApp.controller("myController", myController);
