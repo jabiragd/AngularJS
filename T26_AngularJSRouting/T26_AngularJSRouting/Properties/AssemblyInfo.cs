8af77927e0e5815da1c52ae07b496a7341b0e3a3
JabirAgd <jabiragd@gmail.com>
2021-08-29 16:45:06 +0400

    initial commit

---
+++ T26_AngularJSRouting/T26_AngularJSRouting/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("T26_AngularJSRouting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("T26_AngularJSRouting")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("18d4b3c2-1e21-4119-b041-be92e5a26c3c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ T26_AngularJSRouting/T26_AngularJSRouting/scripts/AppScript.js
@@ -0,0 +1,70 @@
+﻿//var myModule = angular.module("myModule", ["ngRoute"]);
+
+//myModule.config(function ($rootProvider) {
+//    $rootProvider
+//        .when("/home",{
+//            templateUrl: "Templates/home.html",
+//            controller:"homeController"
+//        })
+//        .when("/courses", {
+//            templateUrl: "Templates/courses.html",
+//            controller: "coursesController"
+//        })
+//        .when("/students", {
+//            templateUrl: "Templates/students.html",
+//            controller: "studentsController"
+//        })
+//        .controller("homeController", function ($scope) {
+//            $scope.message = "Home Page";
+//        })
+//        .controller("coursesController", function ($scope) {
+//            $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
+//        })
+//        .controller("studentsController", function ($scope, $http) {
+           
+//            $http.get("StudentService.asmx/GetAllStudents").then(function (response) {
+//                $scope.students = response.data;
+//            })
+
+//        });
+//})
+var app = angular
+            .module("Demo", ["ngRoute"])
+            .config(function ($routeProvider,$locationProvider) {
+                $routeProvider
+                    .when("/home", {
+                        templateUrl: "Templates/home.html",
+                        controller: "homeController"
+                    })
+                    .when("/courses", {
+                        templateUrl: "Templates/courses.html",
+                        controller: "coursesController"
+                    })
+                    .when("/students", {
+                        templateUrl: "Templates/students.html",
+                        controller: "studentsController"
+                    })
+                    .when("/student/:id", {
+                        templateUrl: "Templates/studentdetail.html",
+                        controller: "studentDetailsController"
+                    })
+                    .otherwise({
+                        redirectTo:"/home"
+                    })
+                $locationProvider.html5Mode(true);
+            })
+            .controller("homeController", function ($scope) {
+                $scope.message = "Home Page";
+            })
+            .controller("coursesController", function ($scope) {
+                $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
+            })
+            .controller("studentsController", function ($scope, $http) {
+                 $http.get("StudentService.asmx/GetAllStudents")
+                .then(function (response) {
+                    $scope.students = response.data;
+                })
+            }).controller("studentDetailsController", function ($http) {
+
+          
+            })
