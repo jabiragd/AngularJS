4d0843430fbf25f79e556de3a418cb676aece9b1
JabirAgd <jabirajwan@gmail.com>
2021-08-30 16:56:30 +0300

    initial Commit

---
+++ T21-1_CustomService/T21-1_CustomService/scripts/AppScript.js
@@ -0,0 +1,16 @@
+﻿var myApp = angular.module("myModule", []);
+
+var myController = function ($scope, HttpGETService) {
+    $scope.message = "Angular Test 20_1";
+    HttpGETService.getHttpRespose("EmployeeService.asmx/GetAllEmployees").then(function (response) {
+        $scope.employees = response.data;
+    });
+}
+
+myApp.service("HttpGETService", function ($http) {
+    this.getHttpRespose = function (url) {
+        return $http.get(url, { cache: false });
+    }
+});
+
+myApp.controller("myController", myController);
