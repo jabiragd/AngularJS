1d56bb26248b63af7a071a5dcd430681ae79a529
JabirAgd <jabirajwan@gmail.com>
2021-08-30 21:19:20 +0300

    implementation

---
+++ T27_PageRouting/T27_PageRouting/StudentService.asmx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+
+namespace T27_PageRouting
+{
+    /// <summary>
+    /// Summary description for StudentService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class StudentService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public void GetAllStudents()
+        {
+            List<Student> studentList = new List<Student>();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("",con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Student student = new Student();
+                    student.id = Convert.ToInt32(rdr["id"]);
+                    student.name = rdr["name"].ToString();
+                    student.gender = rdr["gender"].ToString();
+                    student.city = rdr["city"].ToString();
+
+                    studentList.Add(student);
+                }
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(studentList));
+
+        }
+    }
+}
---
+++ T27_PageRouting/T27_PageRouting/scripts/AppScript.js
@@ -0,0 +1,44 @@
+﻿var myApp = angular.module("Demo", ["ngRoute"])
+    .config(function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when("/home", {
+                templateUrl: "Templates/home.html",
+                controller: "homeController"
+            })
+            .when("/courses", {
+                templateUrl: "Templates/courses.html",
+                controller: "coursesController"
+            })
+            .when("/students", {
+                templateUrl: "Templates/students.html",
+                controller: "studentsController"
+            })
+            
+    })
+    .controller("homeController", homeController)
+    .controller("coursesController", function ($scope) {
+        $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
+    })
+    .controller("studentsController", function ($scope, $http) {
+        $http.get("StudentService.asmx/GetAllStudents")
+                            .then(function (response) {
+                                $scope.students = response.data;
+                            })
+    })
+        //.controller("homeController", homeController)
+        //.controller("coursesController", coursesController)
+        //.controller("studentsController", studentsController)
+
+
+var homeController = function ($scope) {
+    $scope.message = "WELCOME TUTORIAL";
+}
+
+var coursesController = function ($scope) {
+    $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
+}
+
+var studentsController = function ($scope,$http) {
+    
+}
