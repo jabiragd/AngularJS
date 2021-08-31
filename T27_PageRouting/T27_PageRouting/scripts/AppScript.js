866b025907e209c5eee91c10946bf21061b4d317
JabirAgd <jabiragd@gmail.com>
2021-08-31 16:40:33 +0400

    implementation

--- T27_PageRouting/T27_PageRouting/scripts/AppScript.js
+++ T27_PageRouting/T27_PageRouting/scripts/AppScript.js
@@ -4,46 +4,56 @@
         $routeProvider
             .when("/home", {
                 templateUrl: "Templates/home.html",
-                controller: "homeController"
+                //controller: "homeController as homeCtrl"
+                controller: "homeController",
+                controllerAs: "homeCtrl"
             })
             .when("/courses", {
                 templateUrl: "Templates/courses.html",
-                controller: "coursesController"
+                //controller: "coursesController as coursesCtrl"
+                controller: "coursesController",
+                controllerAs: "coursesCtrl"
             })
             .when("/students", {
                 templateUrl: "Templates/students.html",
-                controller: "studentsController"
+                //controller: "studentsController as studentCtrl"
+                controller: "studentsController",
+                controllerAs: "studentCtrl"
             })
             .when("/students/:id", {
                 templateUrl: "Templates/studentDetails.html",
-                controller: "studentDetailsController"
+                //controller: "studentDetailsController as studentDetailsCtrl"
+                controller: "studentDetailsController",
+                controllerAs: "studentDetailsCtrl"
             })
             .otherwise({
                 redirectTo:"/home"
             })
             
     })
-    .controller("homeController", function ($scope) {
-        $scope.message = "welcome message";
+    .controller("homeController", function () {
+        this.message = "welcome message";
     })
-    .controller("coursesController", function ($scope) {
-        $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
+    .controller("coursesController", function () {
+        this.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
     })
-    .controller("studentsController", function ($scope, $http) {
+    .controller("studentsController", function ($http) {
         //StudentService.asmx/GetAllStudents
+        var vm = this;
         $http.get("StudentService.asmx/GetAllStudents")
                             .then(function (response) {
-                                $scope.students = response.data;
+                                vm.students = response.data;
         })
     })
-    .controller("studentDetailsController", function ($scope, $http, $routeParams) {
+    .controller("studentDetailsController", function ($http, $routeParams) {
+        var vm = this;
         $http({
             url: "StudentService.asmx/GetStudent",
             method: "get",
             params: { id: $routeParams.id }
         })
         .then(function (response) {
-            $scope.student = response.data;
+            vm.student = response.data;
         }, function (reason) {
             console.log(reason)
         })
