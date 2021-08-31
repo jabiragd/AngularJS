e6aeb1cfe3013d353690e76f73c7c52e035ef306
JabirAgd <jabirajwan@gmail.com>
2021-08-31 21:35:47 +0300

    initial commit

---
+++ T36_RouteReload/T36_RouteReload/StudentService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="StudentService.asmx.cs" Class="T36_RouteReload.StudentService" %>
---
+++ T36_RouteReload/T36_RouteReload/scripts/AppScript.js
@@ -0,0 +1,28 @@
+﻿var myApp = angular.module("myModule", ["ngRoute"])
+            .config(function ($routeProvider) {
+                $routeProvider
+                .when({
+                    templateUrl: "Templates/home.html",
+                    controller:"homeController"
+                })
+                .when({
+                    templateUrl: "Templates/courses.html",
+                    controller:"coursesController"
+                })
+                .when({
+                    templateUrl: "Templates/students.html",
+                    controller: "studentsController"
+                })
+            })
+            .controller("homeController", function ($scope) {
+                $scope.message = "WELCOME";
+            })
+            .controller("coursesController", function ($scope) {
+                $scope.courses = ["C#","JAVA","VB","KOTLIN"];
+            })
+            .controller("studentsController", function ($scope,$http) {
+                $http.get("").then(function (response) {
+                    $scope.students = response.data;
+                })
+            })
+
