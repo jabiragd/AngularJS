c3f4c7ed2ae64d1f17be79475db9559898ff7e24
JabirAgd <jabiragd@gmail.com>
2021-08-31 17:03:25 +0400

    initial commit

---
+++ T34_Controller-Scope/T34_Controller-Scope/scripts/AppScript.js
@@ -0,0 +1,21 @@
+﻿//var myApp = angular.module("myModule", [])
+//            .controller("countryController", function ($scope) {
+//                $scope.name = "India";
+//            })
+//            .controller("stateController", function ($scope) {
+//                $scope.name = "TamilNadu";
+//            })
+//            .controller("cityController", function ($scope) {
+//                $scope.name = "Chennai";
+//            });
+
+var myApp = angular.module("myModule", [])
+            .controller("countryController", function () {
+                this.name = "India";
+            })
+            .controller("stateController", function () {
+                this.name = "TamilNadu";
+            })
+            .controller("cityController", function () {
+                this.name = "Chennai";
+            });
