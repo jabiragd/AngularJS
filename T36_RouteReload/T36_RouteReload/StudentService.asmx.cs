4e8eb88ad58310af7cf6e4fa631e947381a9d740
JabirAgd <jabiragd@gmail.com>
2021-09-01 15:06:42 +0400

    implementation

--- T36_RouteReload/T36_RouteReload/StudentService.asmx.cs
+++ T36_RouteReload/T36_RouteReload/StudentService.asmx.cs
@@ -31,7 +31,7 @@ namespace T36_RouteReload
         {
             List<Student> studentlist = new List<Student>();
 
-            string cs = ConfigurationManager.ConnectionStrings[""].ConnectionString;
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
 
             using (SqlConnection con = new SqlConnection(cs))
             {
@@ -54,6 +54,43 @@ namespace T36_RouteReload
 
             JavaScriptSerializer js = new JavaScriptSerializer();
             Context.Response.Write(js.Serialize(studentlist));
+        }
+
+        [WebMethod]
+        public void GetStudentsByName(string name)
+        {
+            List<Student> studentsList = new List<Student>();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from  tblStudents where name like @name", con);
+
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@name",
+                    Value = name + "%"
+                };
+                cmd.Parameters.Add(param);
+
+                con.Open();
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Student student = new Student();
+                    student.id = Convert.ToInt32(rdr["id"]);
+                    student.name = rdr["name"].ToString();
+                    student.gender = rdr["gender"].ToString();
+                    student.city = rdr["city"].ToString();
+
+                    studentsList.Add(student);
+                }
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(studentsList));
         }
     }
 }
--- T36_RouteReload/T36_RouteReload/scripts/AppScript.js
+++ T36_RouteReload/T36_RouteReload/scripts/AppScript.js
@@ -1,28 +1,131 @@
 ﻿var myApp = angular.module("myModule", ["ngRoute"])
-            .config(function ($routeProvider) {
+            .config(function ($routeProvider, $locationProvider) {
+                $locationProvider.html5Mode(true);
                 $routeProvider
-                .when({
+                .when("/home",{
                     templateUrl: "Templates/home.html",
-                    controller:"homeController"
+                    controller: "homeController",
+                    controllerAs: "homeCtrl",
+                    caseInsensitiveMatch:true
                 })
-                .when({
+                .when("/courses",{
                     templateUrl: "Templates/courses.html",
-                    controller:"coursesController"
+                    controller: "coursesController",
+                    controllerAs: "coursesCtrl",
+                    caseInsensitiveMatch:true
                 })
-                .when({
+                .when("/students",{
                     templateUrl: "Templates/students.html",
-                    controller: "studentsController"
+                    controller: "studentsController",
+                    controllerAs: "studentCtrl",
+                    caseInsensitiveMatch:true
+                })
+                .when("/studentSearch/:name?", {
+                    templateUrl: "Templates/studentsSearch.html",
+                    controller: "studentsSearchController",
+                    controllerAs: "studentsSearchCtrl"
+                })
+                .otherwise({
+                    redirectTo: "/home"
                 })
             })
-            .controller("homeController", function ($scope) {
-                $scope.message = "WELCOME";
+            .controller("homeController", function () {
+                this.message = "WELCOME";
             })
-            .controller("coursesController", function ($scope) {
-                $scope.courses = ["C#","JAVA","VB","KOTLIN"];
+            .controller("coursesController", function () {
+                this.courses = ["C#", "JAVA", "VB", "KOTLIN"];
             })
-            .controller("studentsController", function ($scope,$http) {
-                $http.get("").then(function (response) {
-                    $scope.students = response.data;
+            .controller("studentsSearchController", function ($http,$routeParams) {
+
+                var vm = this;
+                $http({
+                    url: "StudentService.asmx/GetStudentsByName",
+                    method: "get",
+                    params:{name:$routeParams.name}
                 })
+                .then(function (response) {
+                    vm.students = response.data;
+                })
+            })
+            .controller("studentsController", function ($http,$route,$scope,$log,$location) {
+                var vm = this;
+
+                this.reloadData = function () {
+                    $route.reload();
+                }
+
+                this.studentSearch = function () {
+                    if (vm.name) {
+                        $location.url("/studentSearch/" + vm.name)
+                    } else {
+                        $location.url("/studentSearch/")
+                    }
+                }
+
+
+                //$scope.$on("$routeChangeStart", function (event, next, current) {
+                //    if (!confirm("Are you sure you want to navigate away from this page")) {
+                //        event.preventDefault();
+                //    }
+                //})
+
+
+                //$scope.$on("$routeChangeStart", function (event, next, current) {
+                //    if (!confirm("Are you sure you want to navigate away from this page to " + next.$$route.originalPath)) {
+                //        event.preventDefault();
+                //    }
+                //})
+
+                //$scope.$on("$locationChangeStart", function (event, next, current) {
+                //    if (!confirm("Are you sure you want to navigate away from this page to " + next)) {
+                //        event.preventDefault();
+                //    }
+                //})
+
+                //$rootScope.$on("$locationChangeStart", function () {
+                //    $log.debug("$locationChangeStart fired");
+                //    console.log("$locationChangeStart fired");
+                //});
+
+                //$rootScope.$on("$routeChangeStart", function () {
+                //    $log.debug("$routeChangeStart fired");
+                //    console.log("$routeChangeStart fired");
+                //});
+
+                //$rootScope.$on("$locationChangeSuccess", function () {
+                //    $log.debug("$locationChangeSuccess fired");
+                //    console.log("$locationChangeSuccess fired");
+                //});
+
+                //$rootScope.$on("$routeChangeSuccess", function () {
+                //    $log.debug("$routeChangeSuccess fired");
+                //    console.log("$routeChangeSuccess fired");
+                //});
+
+                $scope.$on("$locationChangeStart", function () {
+                    $log.debug("$locationChangeStart fired");
+                    console.log("$locationChangeStart fired");
+                });
+
+                $scope.$on("$routeChangeStart", function () {
+                    $log.debug("$routeChangeStart fired");
+                    console.log("$routeChangeStart fired");
+                });
+
+                $scope.$on("$locationChangeSuccess", function () {
+                    $log.debug("$locationChangeSuccess fired");
+                    console.log("$locationChangeSuccess fired");
+                });
+
+                $scope.$on("$routeChangeSuccess", function () {
+                    $log.debug("$routeChangeSuccess fired");
+                    console.log("$routeChangeSuccess fired");
+                });
+
+               
+                $http.get("StudentService.asmx/GetAllStudents").then(function (response) {
+                    vm.students = response.data;
+                })
+
             })
 
