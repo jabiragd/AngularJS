65e2ded0b447589cf066014507572d8c3bf535af
JabirAgd <jabirajwan@gmail.com>
2021-09-01 22:12:40 +0300

    initial commit

--- T41_RouteResolve/T41_RouteResolve/StudentService.asmx.cs
+++ T41_RouteResolve/T41_RouteResolve/StudentService.asmx.cs
@@ -16,7 +16,7 @@ namespace T41_RouteResolve
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+     [System.Web.Script.Services.ScriptService]
     public class StudentService : System.Web.Services.WebService
     {
 
@@ -53,6 +53,79 @@ namespace T41_RouteResolve
                 JavaScriptSerializer js = new JavaScriptSerializer();
                 Context.Response.Write(js.Serialize(studentList));
             }
+        }
+
+        [WebMethod]
+        public void GetStudent(int id)
+        {
+            Student student = new Student();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from tblStudents where id = @id", con);
+
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@id",
+                    Value = id
+                };
+                cmd.Parameters.Add(param);
+                con.Open();
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    student.id = Convert.ToInt32(rdr["id"]);
+                    student.name = rdr["name"].ToString();
+                    student.gender = rdr["gender"].ToString();
+                    student.city = rdr["city"].ToString();
+                }
+
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                Context.Response.Write(js.Serialize(student));
+            }
+        }
+
+        [WebMethod]
+        public void GetStudentsByName(string name)
+        {
+            List<Student> studentList = new List<Student>();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from tblStudents where name like @name", con);
+
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@name",
+                    Value = name +"%"
+                };
+
+                cmd.Parameters.Add(param);
+                con.Open();
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Student student = new Student();
+                    student.id = Convert.ToInt32(rdr["id"]);
+                    student.name = rdr["name"].ToString();
+                    student.gender = rdr["gender"].ToString();
+                    student.city = rdr["city"].ToString();
+
+                    studentList.Add(student);
+                }
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(studentList));
+
         }
     }
 }
--- T41_RouteResolve/T41_RouteResolve/scripts/AppScript.js
+++ T41_RouteResolve/T41_RouteResolve/scripts/AppScript.js
@@ -15,6 +15,14 @@
                     templateUrl: "Templates/students.html",
                     controller: "studentsController"
                 })
+                .when("/students/:id", {
+                    templateUrl: "Templates/studentDetails.html",
+                    controller: "studentDetailsController"
+                })
+                .when("/studentsSearch/:name?",{
+                    templateUrl: "Templates/studentsSearch.html",
+                    controller: "studentsSearchController"
+                })
                 .otherwise({
                     redirectTo: "/home"
                 })
@@ -25,10 +33,45 @@
             .controller("coursesController", function ($scope) {
                 $scope.courses = ["C#","JAVA","C","C++"]
             })
-            .controller("studentsController", function ($scope, $http) {
+            .controller("studentsController", function ($scope, $http,$location) {
 
-                //$http.get("StudentService.asmx/GetAllStudents")
-                //    .then(function (response) {
-                //        $scope.students = response.data;
-                //    })
+                $scope.studentSearch = function () {
+                    console.log("studentSearch")
+                    if ($scope.name) {
+                        $location.url("/studentsSearch/" + $scope.name)
+                    } else {
+                        $location.url("/studentsSearch/")
+                    }
+                }
+
+                $http.get("StudentService.asmx/GetAllStudents")
+                    .then(function (response) {
+                        $scope.students = response.data;
+                    })
+            })
+            .controller("studentDetailsController", function ($scope, $http, $routeParams) {
+                $http({
+                    url: "StudentService.asmx/GetStudent",
+                    method: "get",
+                    params: { id: $routeParams.id}
+                }).then(function (response) {
+                    $scope.student = response.data;
+                })
+            })
+            .controller("studentsSearchController", function ($scope, $http, $routeParams) {
+
+                if($routeParams.name){
+                    $http({
+                        url: "StudentService.asmx/GetStudentsByName",
+                        method: "get",
+                        params: { name: $routeParams.name }
+                    }).then(function (response) {
+                        $scope.students = response.data;
+                    })
+                } else {
+                    $http.get("StudentService.asmx/GetAllStudents")
+                    .then(function (response) {
+                        $scope.students = response.data;
+                    })
+                }
             })
