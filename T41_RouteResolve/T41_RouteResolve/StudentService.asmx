0dcd4d2b57883542ef10093c8b0811b12a2cab2a
JabirAgd <jabiragd@gmail.com>
2021-09-01 17:13:47 +0400

    implementaion

---
+++ T41_RouteResolve/T41_RouteResolve/StudentService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="StudentService.asmx.cs" Class="T41_RouteResolve.StudentService" %>
---
+++ T41_RouteResolve/T41_RouteResolve/scripts/AppScript.js
@@ -0,0 +1,34 @@
+﻿var myApp = angular.module("myModule", ["ngRoute"])
+            .config(function ($routeProvider,$locationProvider)
+            {
+                $locationProvider.html5Mode(true);
+                $routeProvider
+                .when("/home", {
+                    templateUrl: "Templates/home.html",
+                    controller: "homeController"
+                })
+                .when("/courses", {
+                    templateUrl: "Templates/courses.html",
+                    controller: "coursesController"
+                })
+                .when("/students", {
+                    templateUrl: "Templates/students.html",
+                    controller: "studentsController"
+                })
+                .otherwise({
+                    redirectTo: "/home"
+                })
+            })
+            .controller("homeController", function ($scope) {
+                $scope.message = "WELCOME";
+            })
+            .controller("coursesController", function ($scope) {
+                $scope.courses = ["C#","JAVA","C","C++"]
+            })
+            .controller("studentsController", function ($scope, $http) {
+
+                //$http.get("StudentService.asmx/GetAllStudents")
+                //    .then(function (response) {
+                //        $scope.students = response.data;
+                //    })
+            })
